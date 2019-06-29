5a169ce5136498fe6ed20a14769dee4aec66e86f
Isangeles <dev@isangeles.pl>
2019-06-29 21:07:12 +0200

    crafting: result name & amount for recipe description, removing required items from PC inventory

--- crafting.go
+++ crafting.go
@@ -24,134 +24,139 @@
 package main
 
 import (
-  "bufio"
-  "fmt"
-  "os"
-  "strconv"
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
 
-	"github.com/isangeles/flame/core/data/text/lang"
-	"github.com/isangeles/flame/core/module/object/craft"
-	"github.com/isangeles/flame/core/module/object/character"
 	flameconf "github.com/isangeles/flame/config"
+	"github.com/isangeles/flame/core/data/text/lang"
+	"github.com/isangeles/flame/core/module/object/character"
+	"github.com/isangeles/flame/core/module/object/craft"
 )
 
 // craftingDialog starts CLI dialog for
 // active PC crafting.
 func craftingDialog() error {
-  langPath := flameconf.LangPath()
-  if game == nil {
+	langPath := flameconf.LangPath()
+	if game == nil {
 		msg := lang.TextDir(langPath, "no_game_err")
 		return fmt.Errorf(msg)
-  }
-  if activePC == nil {
-    msg := lang.TextDir(langPath, "no_pc_err")
-    return fmt.Errorf(msg)
-  }
-  for {
-    // Select recipe.
-    recipe, err := recipeDialog(activePC)
-    if err != nil {
-      fmt.Printf("%v\n", err)
-      break
-    }
-    // Print recipe details.
-    fmt.Printf("%s:\t%s\n", lang.TextDir(langPath, "crafting_recipe"),
-      recipe.ID())
-    fmt.Printf("%s:\t%s\n", lang.TextDir(langPath, "crafting_category"),
-      recipe.CategoryID())
-    // TODO: print result & amount.
-    // Recipe options.
-    ans := 0
-    for ans == 0 {
-      // Print options.
-      fmt.Printf("[%d]%s\n", 1, lang.TextDir(langPath, "crafting_make"))
-      fmt.Printf("[%d]%s\n", 2, lang.TextDir(langPath, "dialog_back"))
-      fmt.Printf("[%d]%s\n", 3, lang.TextDir(langPath, "dialog_cancel"))
-      // Scan input.
-      scan := bufio.NewScanner(os.Stdin)
-      scan.Scan()
-      input := scan.Text()
-      n, err := strconv.Atoi(input)
-      if err != nil {
-        msg := lang.TextDir(langPath, "nan_err")
-        fmt.Printf("%s:%v\n", msg, err)
-        continue
-      }
-      if n < 1 || n > 3 {
-        msg := lang.TextDir(langPath, "invalid_input_err")
-        fmt.Printf("%s:%s\n", msg, input)
-        continue
-      }
-      ans = n
-    }
-    if ans == 2 {
-      continue
-    }
-    if ans == 3 {
-      break
-    }
-    if ans == 1 {
-      err := pcMakeRecipe(recipe)
-      if err != nil {
-        fmt.Printf("%v\n", err)
-        continue
-      }
-      break
-    }
-  }
-  return nil
+	}
+	if activePC == nil {
+		msg := lang.TextDir(langPath, "no_pc_err")
+		return fmt.Errorf(msg)
+	}
+	for {
+		// Select recipe.
+		recipe, err := recipeDialog(activePC)
+		if err != nil {
+			fmt.Printf("%v\n", err)
+			break
+		}
+		// Print recipe details.
+		fmt.Printf("%s:\t%s\n", lang.TextDir(langPath, "crafting_recipe"),
+			recipe.ID())
+		fmt.Printf("%s:\t%s\n", lang.TextDir(langPath, "crafting_category"),
+			recipe.CategoryID())
+		fmt.Printf("%s:\n", lang.TextDir(langPath, "crafting_result"))
+		for _, r := range recipe.Result() {
+			fmt.Printf("\t%s\tx%d\n", r.ID, r.Amount)
+		}
+		// Recipe options.
+		ans := 0
+		for ans == 0 {
+			// Print options.
+			fmt.Printf("[%d]%s\n", 1, lang.TextDir(langPath, "crafting_make"))
+			fmt.Printf("[%d]%s\n", 2, lang.TextDir(langPath, "dialog_back"))
+			fmt.Printf("[%d]%s\n", 3, lang.TextDir(langPath, "dialog_cancel"))
+			// Scan input.
+			scan := bufio.NewScanner(os.Stdin)
+			scan.Scan()
+			input := scan.Text()
+			n, err := strconv.Atoi(input)
+			if err != nil {
+				msg := lang.TextDir(langPath, "nan_err")
+				fmt.Printf("%s:%v\n", msg, err)
+				continue
+			}
+			if n < 1 || n > 3 {
+				msg := lang.TextDir(langPath, "invalid_input_err")
+				fmt.Printf("%s:%s\n", msg, input)
+				continue
+			}
+			ans = n
+		}
+		if ans == 2 {
+			continue
+		}
+		if ans == 3 {
+			break
+		}
+		if ans == 1 {
+			err := pcMakeRecipe(recipe)
+			if err != nil {
+				fmt.Printf("%v\n", err)
+				continue
+			}
+			break
+		}
+	}
+	return nil
 }
 
 // recipeDialog starts recipe dialog for specified
 // game character.
 func recipeDialog(c *character.Character) (*craft.Recipe, error) {
-  langPath := flameconf.LangPath()
-  recipes := c.Recipes()
-  if len(recipes) < 1 {
-    msg := lang.TextDir(langPath, "crafting_no_recipes_err")
-    return nil, fmt.Errorf(msg)
-  }
-  var recipe *craft.Recipe
-  for recipe == nil {
-    // List recipes.
-    fmt.Printf("%s:\n", lang.TextDir(langPath, "crafting_recipes"))
-    for i, r := range recipes {
-      fmt.Printf("[%d]%s\t%s\n", i, r.ID(), r.CategoryID())
-    }
-    // Select ID.
-    fmt.Printf("%s:", lang.TextDir(langPath, "crafting_select_recipe"))
-    // Scan input.
-    scan := bufio.NewScanner(os.Stdin)
-    scan.Scan()
-    input := scan.Text()
-    id, err := strconv.Atoi(input)
-    if err != nil {
-      fmt.Printf("%s:%v\n", lang.TextDir(langPath, "nan_err"), input)
-      continue
-    }
-  	if id < 0 || id > len(recipes)-1 {
-  		fmt.Printf("%s:%s\n", lang.TextDir(langPath, "invalid_input_err"), input)
-  		continue
-  	}
-    recipe = recipes[id]
-  }
-  return recipe, nil
+	langPath := flameconf.LangPath()
+	recipes := c.Recipes()
+	if len(recipes) < 1 {
+		msg := lang.TextDir(langPath, "crafting_no_recipes_err")
+		return nil, fmt.Errorf(msg)
+	}
+	var recipe *craft.Recipe
+	for recipe == nil {
+		// List recipes.
+		fmt.Printf("%s:\n", lang.TextDir(langPath, "crafting_recipes"))
+		for i, r := range recipes {
+			fmt.Printf("[%d]%s\t%s\n", i, r.ID(), r.CategoryID())
+		}
+		// Select ID.
+		fmt.Printf("%s:", lang.TextDir(langPath, "crafting_select_recipe"))
+		// Scan input.
+		scan := bufio.NewScanner(os.Stdin)
+		scan.Scan()
+		input := scan.Text()
+		id, err := strconv.Atoi(input)
+		if err != nil {
+			fmt.Printf("%s:%v\n", lang.TextDir(langPath, "nan_err"), input)
+			continue
+		}
+		if id < 0 || id > len(recipes)-1 {
+			fmt.Printf("%s:%s\n", lang.TextDir(langPath, "invalid_input_err"), input)
+			continue
+		}
+		recipe = recipes[id]
+	}
+	return recipe, nil
 }
 
 // pcMakeRecipe checks requirements and makes
 // items from specified recipe for active PC.
 func pcMakeRecipe(r *craft.Recipe) error {
-  langPath := flameconf.LangPath()
-  if activePC == nil {
-    msg := lang.TextDir(langPath, "no_pc_err")
-    return fmt.Errorf(msg)
-  }
-  if !activePC.MeetReqs(r.Reqs()...) {
-    msg := lang.TextDir(langPath, "reqs_not_meet")
-    return fmt.Errorf(msg)
-  }
-  for _, i := range r.Make() {
-    activePC.Inventory().AddItem(i)
-  }
-  return nil
+	langPath := flameconf.LangPath()
+	if activePC == nil {
+		msg := lang.TextDir(langPath, "no_pc_err")
+		return fmt.Errorf(msg)
+	}
+	if !activePC.MeetReqs(r.Reqs()...) {
+		msg := lang.TextDir(langPath, "reqs_not_meet")
+		return fmt.Errorf(msg)
+	}
+	activePC.ChargeReqs(r.Reqs()...)
+	res := r.Make()
+	for _, i := range res {
+		activePC.Inventory().AddItem(i)
+	}
+	return nil
 }

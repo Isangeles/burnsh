73f3971a5f5cbcb279221b8d4262e92422961c02
Isangeles <dev@isangeles.pl>
2019-07-17 17:08:16 +0200

    train: printing informations about training

--- train.go
+++ train.go
@@ -33,6 +33,7 @@ import (
 	"github.com/isangeles/flame/core/data/text/lang"
 	char "github.com/isangeles/flame/core/module/object/character"
 	"github.com/isangeles/flame/core/module/train"
+	"github.com/isangeles/flame/core/module/req"
 )
 
 // tradeDialog starts CLI dialog for train
@@ -84,7 +85,12 @@ func selectTraining(trainings []train.Training) train.Training {
 		// List available trainings.
 		fmt.Printf("%s:\n", lang.TextDir(langPath, "train_trainings"))
 		for i, t := range trainings {
-			fmt.Printf("\t[%d]%v\n", i, t)
+			fmt.Printf("\t[%d]%s\n", i, trainingInfo(t))
+			// List training reqs.
+			fmt.Printf("\t%s:\n", lang.TextDir(langPath, "train_reqs"))
+			for _, r := range t.Reqs() {
+				fmt.Printf("\t%s\n", reqInfo(r))
+			}
 		}
 		fmt.Printf("%s:", lang.TextDir(langPath, "train_select_training"))
 		// Scan input.
@@ -100,11 +106,66 @@ func selectTraining(trainings []train.Training) train.Training {
 			continue
 		}
 		if id < 0 || id > len(trainings)-1 {
-			fmt.Printf("%s:%s\n", lang.TextDir(langPath, "invalid_input_err"),
-				input)
+			fmt.Printf("%s:%s\n", lang.TextDir(langPath,
+				"invalid_input_err"), input)
 			continue
 		}
 		training = trainings[id]
 	}
 	return training
+}
+
+// trainingInfo returns information about
+// training do display.
+func trainingInfo(t train.Training) string {
+	langPath := flameconf.LangPath()
+	// Train info.
+	info := ""
+	switch t := t.(type) {
+	case *train.AttrsTraining:
+		info = fmt.Sprintf("%s", lang.TextDir(langPath,
+			"train_attrs_training"))
+		if t.Strenght() > 0 {
+			strLabel := lang.TextDir(langPath, "attr_str")
+			info = fmt.Sprintf("%s: %s(%d)", info, strLabel, t.Strenght())
+		}
+		if t.Constitution() > 0 {
+			conLabel := lang.TextDir(langPath, "attr_con")
+			info = fmt.Sprintf("%s: %s(%d)", info, conLabel, t.Constitution())
+		}
+		if t.Dexterity() > 0 {
+			dexLabel := lang.TextDir(langPath, "attr_dex")
+			info = fmt.Sprintf("%s: %s(%d)", info, dexLabel, t.Dexterity())
+		}
+		if t.Wisdom() > 0 {
+			wisLabel := lang.TextDir(langPath, "attr_wis")
+			info = fmt.Sprintf("%s: %s(%d)", info, wisLabel, t.Wisdom())
+		}
+		if t.Intelligence() > 0 {
+			intLabel := lang.TextDir(langPath, "attr_int")
+			info = fmt.Sprintf("%s: %s(%d)", info, intLabel, t.Intelligence())
+		}
+	default:
+		info = fmt.Sprintf("%s%s", info, "unknown")
+	}
+	return info
+}
+
+// reqInfo returns information about specified
+// requirement.
+func reqInfo(r req.Requirement) string {
+	langPath := flameconf.LangPath()
+	info := ""
+	switch r := r.(type) {
+	case *req.ItemReq:
+		reqLabel := lang.TextDir(langPath, "req_item")
+		info = fmt.Sprintf("%s: %s x%d", reqLabel, r.ItemID(),
+			r.ItemAmount())
+	case *req.CurrencyReq:
+		reqLabel := lang.TextDir(langPath, "req_currency")
+		info = fmt.Sprintf("%s: %d", reqLabel, r.Amount())
+	default:
+		return "unknown"
+	}
+	return info
 }

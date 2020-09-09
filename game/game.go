0bae17bf726c0f0d8e4e6b5b0684d61546b30d5a
Isangeles <dev@isangeles.pl>
2020-09-09 22:19:15 +0200

    game: player struct

--- game/game.go
+++ game/game.go
@@ -25,16 +25,62 @@
 package game
 
 import (
+	"fmt"
+
 	"github.com/isangeles/flame"
+	"github.com/isangeles/flame/module/character"
 )
 
 // Struct for game wrapper.
 type Game struct {
 	*flame.Game
+	players      []*Player
+	activePlayer *Player
 }
 
 // New creates new game wrapper for specified game.
 func New(game *flame.Game) *Game {
 	g := Game{Game: game}
 	return &g
+}
+
+// Players adds new player.
+func (g *Game) Players() []*Player {
+	return g.players
+}
+
+func (g *Game) AddPlayer(char *character.Character) error {
+	player, err := g.newPlayer(char)
+	if err != nil {
+		return fmt.Errorf("Unable to create player: %v", err)
+	}
+	g.players = append(g.players, player)
+	return nil
+}
+
+// ActivePlayer returns active player.
+func (g *Game) ActivePlayer() *Player {
+	return g.activePlayer
+}
+
+// SetActivePlayer sets specified player as active player.
+func (g *Game) SetActivePlayer(player *Player) {
+	g.activePlayer = player
+}
+
+// newPlayer creates new character avatar for the player from specified data and
+// places this character in the start area of game module.
+func (g *Game) newPlayer(char *character.Character) (*Player, error) {
+	// Set start position.
+	char.SetPosition(g.Module().Chapter().Conf().StartPosX,
+		g.Module().Chapter().Conf().StartPosY)
+	// Set start area.
+	startArea := g.Module().Chapter().Area(g.Module().Chapter().Conf().StartArea)
+	if startArea == nil {
+		return nil, fmt.Errorf("game: start area not found: %s",
+			g.Module().Chapter().Conf().StartArea)
+	}
+	startArea.AddCharacter(char)
+	player := Player{char, g}
+	return &player, nil
 }
